c07dcff8dc98b779940fa371144315b58c2c2d10
Saimun Hasan <saimunhasanrifat14@gmail.com>
2024-12-25 02:28:02 +0600

    complate the project

--- js/main.js
+++ js/main.js
@@ -4,11 +4,8 @@ let rendomNumber = Math.round(Math.random() * 20)
 let againBtn = document.getElementsByClassName(".headerTopAgainBtn")
 const againButton = document.querySelector('.headerTopAgainBtn');
 let score = document.querySelector("#score");
+let highscore = document.querySelector("#highscore");
 let scoreCount = 20;
-
-againButton.addEventListener('click', function() {
-  location.reload();
-});
 
 form.addEventListener("submit", () => {
     const userinputValue = +userinput.value
@@ -22,11 +19,16 @@ form.addEventListener("submit", () => {
         document.querySelector("body").style.backgroundColor = "#2dc000"
         document.querySelector(".guessing").innerHTML = "🥳 Correct Number"
         document.querySelector(".number").innerHTML = rendomNumber
+        highscore.innerHTML = scoreCount - 1;
     }
     scoreCount--;
     score.textContent = scoreCount
     userinput.value = ''
-})
+});
 
-
-
+againButton.addEventListener('click', function() {
+    document.querySelector(".guessing").innerHTML = "Start guessing...";
+    document.querySelector("body").style.backgroundColor = "#181818"
+    document.querySelector(".number").innerHTML = "?"
+    score.textContent = 20;
+});
